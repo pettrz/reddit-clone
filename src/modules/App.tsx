d686fb1bc71aafa9832ec6d16a4956ad7ff56f40
Petter Knutsson <petter.knutsson5@gmail.com>
2019-08-20 21:28:57 +0200

    created grid layout, added flexbox mixins

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <header>Header content</header>
+      <aside>Aside content</aside>
+      <main>Main content</main>
+      <footer>Footer content</footer>
     </div>
   );
 }
