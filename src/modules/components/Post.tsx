9cf547bff9b46eba5448c8581cde31d3cec5c9ac
Petter Knutsson <petter.knutsson5@gmail.com>
2019-08-26 19:19:15 +0200

    added author+creation date data

--- src/modules/components/Post.tsx
+++ src/modules/components/Post.tsx
@@ -4,14 +4,26 @@ import './Post.scss';
 
 const Post = ({ data }: any) => {
   console.log(data);
-  const title = _.get(data.data, 'title', null);
+  const title = _.get(data.data, 'title', '');
   const thumbnail = _.get(data.data, 'thumbnail', '');
-  // const created = _.get(data.data, 'created', null);
-  // const author = _.get(data.data, 'author', null);
+  const created =  _.get(data.data, 'created', 0);
+  const creationDate = new Date(created * 1000).toDateString()
+  const author = _.get(data.data, 'author', '');
+  console.log(created);
   return (
     <div className="post row">
-      <div className="post__thumbnail"><img src={thumbnail} alt=""/></div>
-      <h5>{title}</h5>
+      <div className='post__date-posted'>
+        {creationDate}
+      </div>
+      <div className="post__thumbnail">
+        <img src={thumbnail} alt="" />
+      </div>
+      <div className="post__body">
+        <h5>{title}</h5>
+        <div className="post__body__author">
+          Submitted by {author}
+        </div>
+      </div>
     </div>
   );
 };
