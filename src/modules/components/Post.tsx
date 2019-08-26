d6972244e95f49c593bb3ec53d515c52bc3d045e
Petter Knutsson <petter.knutsson5@gmail.com>
2019-08-26 20:48:46 +0200

    added temporary standard thumbnail for posts without thumbnail

--- src/modules/components/Post.tsx
+++ src/modules/components/Post.tsx
@@ -6,23 +6,23 @@ const Post = ({ data }: any) => {
   console.log(data);
   const title = _.get(data.data, 'title', '');
   const thumbnail = _.get(data.data, 'thumbnail', '');
-  const created =  _.get(data.data, 'created', 0);
-  const creationDate = new Date(created * 1000).toDateString()
+  const thumbnailDisplay = thumbnail === 'default' 
+  ? 'https://via.placeholder.com/150' 
+  : thumbnail === 'self' 
+  ? 'https://via.placeholder.com/150' 
+  : thumbnail;
+  const created = _.get(data.data, 'created', 0);
+  const creationDate = new Date(created * 1000).toDateString();
   const author = _.get(data.data, 'author', '');
-  console.log(created);
   return (
     <div className="post row">
-      <div className='post__date-posted'>
-        {creationDate}
-      </div>
+      <div className="post__date-posted">{creationDate}</div>
       <div className="post__thumbnail">
-        <img src={thumbnail} alt="" />
+        <img src={thumbnailDisplay} alt="" />
       </div>
       <div className="post__body">
         <h5>{title}</h5>
-        <div className="post__body__author">
-          Submitted by {author}
-        </div>
+        <div className="post__body__author">Submitted by {author}</div>
       </div>
     </div>
   );
