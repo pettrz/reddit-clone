218f435497f4be76b70156398562a73e9a86be37
Petter Knutsson <petter.knutsson5@gmail.com>
2019-08-27 20:59:30 +0200

    refactored app component, hooks + nextPage function

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -1,41 +1,51 @@
 import axios from 'axios';
-import React from 'react';
+// import _ from 'lodash';
+// å 
+import queryString from 'query-string';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Post from './components/Post';
 
-export default class App extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
+export const App = () => {
+  const [postData, setPostData] = useState([]);
+  const [after, setAfter] = useState('');
+  const [afterQuery, setAfterQuery] = useState('');
 
-    this.state = {
-      kind: '',
-      data: [],
+  useEffect(() => {
+    let ignore = false;
+    const query = {
+      after
     };
-  }
-  componentDidMount() {
-    axios
-      .get('https://www.reddit.com/r/reactjs.json')
-      .then(({ data }) => {
-        console.log(data);
-        this.setState({
-          kind: data.kind,
-          data: data.data.children,
-        });
-      })
-      .catch(error => console.log(error));
-  }
-  render() {
-    return (
-      <div className="wrapper">
-        <header>
-          <h1>Reddit clone</h1>
-        </header>
-        <main>
-          {this.state.data.map((data: any, i: any) => (
-            <Post data={data} key={i} />
-          ))}
-        </main>
-      </div>
-    );
-  }
-}
+    console.log(queryString.stringify(query));
+    async function fetchData() {
+      const result = await axios(`https://www.reddit.com/r/reactjs.json?${queryString.stringify(query)}`);
+      if (!ignore) { 
+        setPostData(result.data.data.children);
+        setAfterQuery(result.data.data.after);
+      }
+    }
+
+    fetchData();
+    return () => { ignore = true; };
+  }, [after]);
+
+  const nextPage = () => setAfter(afterQuery);
+  
+  return (
+    <div className="wrapper">
+      <header>
+        <h1>Reddit clone</h1>
+      </header>
+      <main>
+        {postData.map((data: [], i: number) => (
+          <Post data={data} key={i} />
+        ))}
+        <button onClick={nextPage}>Next</button>
+      </main>
+    </div>
+  );
+};
+
+export default App;
+
+
--- src/modules/components/Post.tsx
+++ src/modules/components/Post.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import './Post.scss';
 
 const Post = ({ data }: any) => {
-  console.log(data);
   const title = _.get(data.data, 'title', '');
   const thumbnail = _.get(data.data, 'thumbnail', '');
-  const thumbnailDisplay = thumbnail === 'default' 
-  ? 'https://via.placeholder.com/150' 
-  : thumbnail === 'self' 
-  ? 'https://via.placeholder.com/150' 
-  : thumbnail;
+  const thumbnailDisplay =
+    thumbnail === 'default'
+      ? 'https://via.placeholder.com/150'
+      : thumbnail === 'self'
+      ? 'https://via.placeholder.com/150'
+      : thumbnail;
   const created = _.get(data.data, 'created', 0);
   const creationDate = new Date(created * 1000).toDateString();
   const author = _.get(data.data, 'author', '');
